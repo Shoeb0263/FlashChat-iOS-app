a75c36a29268deebe7abe0063560e8c515f9e48a
Shoeb0263 <103112482+Shoeb0263@users.noreply.github.com>
2024-01-14 17:17:04 +0530

    Add files via upload

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Shoeb's Macbook on 25/12/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+struct Message{
+    let sender: String
+    let body: String
+}
